1b278fd768076d24ea7c7022052714d2ca1b2287
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-26 05:10:08 +0530

    Calculate Fare for Multiple Rides

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -4,11 +4,18 @@ public class InvoiceGenerator {
     public int MIN_FARE = 5;
     double totalFare;
 
-    public double CalculateFare(double DISTANCE, int TIME) {
-
-        totalFare = COST_PER_KM * DISTANCE + COST_PER_MINUTE * TIME;
+    public double calculateFare(double distance, int time) {
+        totalFare = COST_PER_KM * distance + COST_PER_MINUTE * time;
         if(totalFare < MIN_FARE)
             totalFare = MIN_FARE;
         return totalFare;
+    }
+
+    public double calculateFareForMultipleRides(Ride[] rides) {
+        double aggregateTotalFare = 0;
+        for (Ride ride: rides) {
+            aggregateTotalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return aggregateTotalFare;
     }
 }
--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -2,17 +2,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
     @Test
     void givenDistanceAndTimeShouldReturnFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double Fare = invoiceGenerator.CalculateFare(7.0, 8);
+        double Fare = invoiceGenerator.calculateFare(7.0, 8);
         Assertions.assertEquals(78, Fare);
     }
 
     @Test
     void givenDistanceAndTimeShouldReturnMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double TotalFare = invoiceGenerator.CalculateFare(0.5, 3);
+        double TotalFare = invoiceGenerator.calculateFare(0.5, 3);
         Assertions.assertEquals(8, TotalFare);
+    }
+
+    @Test
+    void givenMltipleRidesShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                        new Ride(0.1, 1)};
+        Assertions.assertEquals(30, invoiceGenerator.calculateFareForMultipleRides(rides));
     }
 }
